ba4c72b5beb5e62c4e61311d7083782d366d5ba0
Mirosław Gucwa <mgucwa@gmail.com>
2022-07-14 17:06:07 +0200

    Fix: formatFullname.js

---
+++ utils/formatFullname.js
@@ -0,0 +1,23 @@
+module.exports = (fullName) => {
+
+  // fullName = fullName.toLowerCase();
+  // console.log('fullName:', fullName);
+
+  if (fullName === '') return 'Error';
+  if (typeof fullName !== 'string') return 'Error';
+
+  const found = fullName.match(/ /g);
+
+  if (!found) return 'Error';
+  if (found.length > 1) return 'Error';
+
+  const fullNameLower = fullName.toLowerCase();
+
+  const [ firstName, lastName ] = fullNameLower.split(' ');
+
+  if(!firstName || !lastName) return false;
+
+  return firstName.charAt(0).toUpperCase() + firstName.slice(1)
+          + ' '
+          + lastName.charAt(0).toUpperCase() + lastName.slice(1);
+};
